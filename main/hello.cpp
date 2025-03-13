11d8be78603366b4388970040a944b871f1a4529
Niki-2807 <nikitha.thammaiah2807@gmail.com>
2025-03-13 21:50:01 +0530

    Describe this commit2

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "PES2UG22CS362_NIKITHA have successfully built and run this  \n";
+  cout << "Hello, Jenkins_362\n";
+  cout << "PES2UG22CS362_NIKITHA has successfully built and run this  \n";
   return 0;
 }
